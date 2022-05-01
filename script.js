1e713c4042eac92166083a6e125f002097c7e20d
liaespina <liaespina21@gmail.com>
2022-05-01 19:32:33 -0300

    version1

---
+++ script.js
@@ -0,0 +1,58 @@
+
+    // el alert es para que aparexca el mensaje tipo pop up 
+    alert("hola mundo ")
+    console.log("hola consola ")
+    /*para q, lo que va entre comillas lo que va es una cadena de texto=string et*/
+
+    let alumna = "veronica"
+    alumna = "pablo"
+    let edad = 21
+    console.log( " la alumna que ingresó es" + alumna + "y la edad es" + edad)
+    //buenas practicas para nombrar variables 
+    let nombreApellido ="Lia Espina"  //camelCase
+    let nombre_apellido = "lia ESpina" //snakeCase
+    let NombreApellido= "lia espina" //pascalCase
+    let $nombreApellido
+    let _nombreApellido 
+
+    /* las siguientes formas no vannnnnnnn
+
+    let nombre-apellido 
+    let 1ernombre
+    let var
+
+    */
+
+    //Se recomienda en general usas siempre let antes que var, porque 
+    //poruqe con var es mas facil que te cambien nombres de variables y se pisen sin avisar
+    /////////////TIPOS DE DATOS /////////
+    let str1 = "pablo" //comillas dobles
+    let str2 = 'maria' ///o comillas simmples
+
+    let str3 = str1 + "y" +str2 + "se casan"
+    
+    console.log(str3)
+
+
+    ////OTRA VARIABLE ES NUMBER 
+
+    // 
+    
+    let numero1 = parseFloat(prompt("ingrese el primer numero"))
+    let numero2 = parseFloat(prompt("ingrese el segundo numero"))
+
+
+    float = 3.14
+    //let numero1 = 1
+    //let numero2 = 2
+    let suma= numero1 + numero2
+    let resta= numero2 - numero1
+    let multi= numero1*numero2
+    let divi= numero1/numero2
+
+    console.log("la suma es " + suma)
+    console.log("la resta es " + resta)
+    console.log("la multi es " + multi)
+    console.log("la divi es " + divi)
+
+
